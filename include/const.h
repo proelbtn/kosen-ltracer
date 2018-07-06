5a5c91cc7f5ea12f099c1fafbc1df1fbfcca227c
proelbtn <proelbtn@users.noreply.github.com>
2018-07-06 10:13:54 +0900

    [FIX] fix lcd didn't work

--- include/const.h
+++ include/const.h
@@ -15,14 +15,15 @@
 #define LCD_WIDTH 8
 #define LCD_HEIGHT 2
 
-#define INT_BASE_RESOLUTION (0x1000 - 0x1)
-// lcd_handler is called every TIMER_INT_TIME0 * 0x400 = 16384[us] = 16.384[ms]
+// resolution is 256
+#define INT_BASE_RESOLUTION (0x100 - 0x1)
+// lcd_handler is called every TIMER_INT_TIME0 * 0x100 = 32768[us] = 32.768[ms]
 #define INT_LCD_CALL_CYCLE_FLAG (0x100 - 0x1)
-// ad_handler is called every TIMER_INT_TIME0 * 0x1 = 64[us]
+// ad_handler is called every TIMER_INT_TIME0 * 0x1 = 128[us]
 #define INT_AD_CALL_CYCLE_FLAG (0x1 - 0x1)
-// motor_handler is called every TIMER_INT_TIME0 * 0x8= 512[us]
+// motor_handler is called every TIMER_INT_TIME0 * 0x4 = 512[us]
 #define INT_MOTOR_CALL_CYCLE_FLAG (0x8 - 0x1)
 
 // unit: 1[us]
-// Addition / Subtraction can be called only 800 times in 64[us]
-#define TIMER_INT_TIME0 64
+// Addition / Subtraction can be called only 1600 times in 128[us]
+#define TIMER_INT_TIME0 128
