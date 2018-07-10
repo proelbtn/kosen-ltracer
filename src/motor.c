976c5e0d9bc2ceb0877c0ece2fe727e2837227eb
proelbtn <proelbtn@users.noreply.github.com>
2018-07-10 09:53:32 +0900

    [FIX] fix motor_set_mode

--- src/motor.c
+++ src/motor.c
@@ -24,16 +24,16 @@ void motor_set_mode_left(unsigned char mode) {
 void motor_set_mode_right(unsigned char mode) {
     switch (mode) {
         case STOP:
-            PBDR = (PBDR & ~0x03) | 0x00;
+            PBDR = (PBDR & ~0x0B) | 0x00;
             break;
         case FORWARD:
-            PBDR = (PBDR & ~0x03) | 0x08;
+            PBDR = (PBDR & ~0x0B) | 0x04;
             break;
         case BACKWARD:
-            PBDR = (PBDR & ~0x03) | 0x04;
+            PBDR = (PBDR & ~0x0B) | 0x08;
             break;
         case BRAKE:
-            PBDR = (PBDR & ~0x03) | 0x0B;
+            PBDR = (PBDR & ~0x0B) | 0x0B;
             break;
     }
 }
